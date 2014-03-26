fdcfb958f5f607e2926ea6bf30611d463a404868
Gaëtan Harter <gaetan.harter@inria.fr>
2014-03-26 11:29:26 +0100

    Allow modifiying arguments in parsers

--- iotlabcli/node_parser.py
+++ iotlabcli/node_parser.py
@@ -117,13 +117,13 @@ def command_node(parser_options, request, parser):
         print json.dumps(json.loads(json_update), indent=4, sort_keys=True)
 
 
-def main():
+def main(args=sys.argv[1:]):
     """
     Main command-line execution loop."
     """
     try:
         parser = parse_options()
-        parser_options = parser.parse_args()
+        parser_options = parser.parse_args(args)
         request = rest.Api(
             username=parser_options.username,
             password=parser_options.password,
