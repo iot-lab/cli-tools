352c5755ec1ff36529efaa1290b9afd217e817f5
Gaëtan Harter <gaetan.harter@inria.fr>
2014-08-29 16:52:31 +0200

    Add tests launcher commands

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import sys
 from setuptools import setup
-from iotlabcli import version
+from setuptools.command.test import test as TestCommand
+from iotlabcli import VERSION
 
 SCRIPTS = ['auth-cli', 'experiment-cli', 'node-cli', 'profile-cli']
 
+
 setup(name='iotlabcli',
-      version = version,
+      version=VERSION,
       description='IoT-LAB testbed command-line client',
       author='IoT-LAB Team',
       author_email='admin@iot-lab.info',
@@ -21,4 +24,7 @@ setup(name='iotlabcli',
                    'Environment :: Console',
                    'Topic :: Utilities', ],
       install_requires=['argparse', 'requests<=1.2.3'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      setup_requires=['setuptools-pep8', 'setuptools-lint'],
       )
