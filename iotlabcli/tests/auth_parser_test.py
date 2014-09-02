03f7984a965f229b8ecc2c7231f242afa379e14b
Gaëtan Harter <gaetan.harter@inria.fr>
2014-09-02 15:04:39 +0200

    Complete coverage on auth_parser main function

--- iotlabcli/tests/auth_parser_test.py
+++ iotlabcli/tests/auth_parser_test.py
@@ -2,9 +2,11 @@
 
 """ Test the iotlabcli.auth_parser module """
 
+import sys
 import unittest
 from mock import patch
 
+import iotlabcli
 from iotlabcli import auth_parser
 
 
@@ -23,3 +25,25 @@ class TestStoreCredentials(unittest.TestCase):
         self.assertEquals(1, helpers_m.password_prompt.call_count)
         helpers_m.create_password_file.assert_called_with('super_user',
                                                           'password')
+
+
+@patch('iotlabcli.auth_parser.store_credentials')
+@patch('sys.stderr', sys.stdout)
+class TestMainAuthParser(unittest.TestCase):
+    def test_main(self, store_m):
+        """ Test main function """
+
+        auth_parser.main(['-u', 'super_user'])
+        store_m.assert_called_with('super_user', None)
+
+        auth_parser.main(['-u', 'super_user', '-p' 'password'])
+        store_m.assert_called_with('super_user', 'password')
+
+    def test_main_exceptions(self, store_m):
+        """ Test main function error cases """
+
+        store_m.side_effect = iotlabcli.Error('message')
+        self.assertRaises(SystemExit, auth_parser.main, ['-u', 'error'])
+
+        store_m.side_effect = KeyboardInterrupt()
+        self.assertRaises(SystemExit, auth_parser.main, ['-u', 'ctrl_c'])
