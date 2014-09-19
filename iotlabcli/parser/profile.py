44778a98d345d8fc6d92206157f31173cb5ee680
Gaëtan Harter <gaetan.harter@inria.fr>
2014-09-19 18:58:04 +0200

    Refactor addprofile

--- iotlabcli/parser/profile.py
+++ iotlabcli/parser/profile.py
@@ -6,19 +6,16 @@ import sys
 import argparse
 from argparse import RawTextHelpFormatter
 
-from iotlabcli import Error
 from iotlabcli import helpers
 from iotlabcli import rest
 from iotlabcli import auth
 from iotlabcli.parser import help_msgs
 from iotlabcli.parser import common
-from iotlabcli.profile import Profile, Consumption, Radio, Sensor
+from iotlabcli.profile import ProfileWSN430, ProfileM3
 
 
 def parse_options():
-    """
-    Handle profile-cli command-line opts with argparse
-    """
+    """ Handle profile-cli command-line opts with argparse """
     parent_parser = common.base_parser()
     # We create top level parser
     parser = argparse.ArgumentParser(
@@ -56,7 +53,7 @@ def parse_options():
     add_m3_parser.add_argument(
         '-p', '--power', dest='power_mode', default='dc',
         help='power mode (dc by default)',
-        choices=['dc', 'battery'])
+        choices=ProfileM3.choices['power_mode'])
 
     add_m3_parser.add_argument(
         '-j', '--json', dest='json',
@@ -75,17 +72,17 @@ def parse_options():
         help='voltage measure')
 
     group_m3_consumption.add_argument(
-        '-cpower', action='store_true',
+        '-power', action='store_true',
         help='power measure')
 
     group_m3_consumption.add_argument(
         '-period', dest='period', type=int,
-        choices=[140, 204, 332, 588, 1100, 2116, 4156, 8244],
+        choices=ProfileM3.choices['consumption']['period'],
         help='period measure (us)')
 
     group_m3_consumption.add_argument(
         '-avg', dest='average', type=int,
-        choices=[1, 4, 16, 64, 128, 256, 512, 1024],
+        choices=ProfileM3.choices['consumption']['average'],
         help='average measure')
 
     group_m3_radio = add_m3_parser.add_argument_group(
@@ -97,98 +94,93 @@ def parse_options():
 
     group_m3_radio.add_argument(
         '-channels', dest='channels', nargs='+',
-        type=int, choices=range(11, 27),
-        metavar='{11..26}',
-        help='List of channels (11 to 26)')
+        type=int, choices=ProfileM3.choices['radio']['channels'],
+        metavar='{11..26}', help='List of channels (11 to 26)')
 
     group_m3_radio.add_argument(
-        '-num', dest='num_per_channel',
-        type=int, choices=range(1, 256),
-        metavar='{0..255}',
-        help='Number of measure by channel')
+        '-num', dest='num_per_channel', default=0, type=int,
+        choices=ProfileM3.choices['radio']['num_per_channel'],
+        metavar='{0..255}', help='Number of measure by channel')
 
     group_m3_radio.add_argument(
-        '-rperiod', dest='rperiod',
-        type=int, choices=range(1, 2**16),
-        metavar='{1..65535}',
-        help='period measure')
+        '-rperiod', dest='rperiod', type=int,
+        choices=ProfileM3.choices['radio']['period'],
+        metavar='{1..65535}', help='period measure')
 
+    #
+    # WSN430 profile
+    #
+
+    add_wsn430_parser.add_argument('-n', '--name', required=True,
+                                   help='profile name')
     add_wsn430_parser.add_argument(
-        '-n', '--name', dest='name', required=True,
-        help='profile name')
+        '-j', '--json', action='store_true',
+        help='print profile JSON representation without add it')
 
     add_wsn430_parser.add_argument(
         '-p', '--power',
         dest='power_mode', default='dc',
-        help='power mode (dc by default)', choices=['dc', 'battery'])
+        help='power mode (dc by default)',
+        choices=ProfileWSN430.choices['power_mode'])
 
-    add_wsn430_parser.add_argument(
-        '-j', '--json',
-        dest='json', action='store_true',
-        help='print profile JSON representation without add it')
-
+    # WSN430 Consumption
     group_wsn430_consumption = add_wsn430_parser.add_argument_group(
         'WSN430 consumption measure')
 
     group_wsn430_consumption.add_argument(
-        '-current', action='store_true',
-        help='current measure')
-
-    group_wsn430_consumption.add_argument(
-        '-voltage', action='store_true',
-        help='voltage measure')
+        '-cfreq', dest='cfreq', type=int,
+        choices=ProfileWSN430.choices['consumption']['frequency'],
+        help='frequency measure (ms)')
 
     group_wsn430_consumption.add_argument(
         '-power', action='store_true',
         help='power measure')
-
     group_wsn430_consumption.add_argument(
-        '-cfreq', dest='cfreq', type=int,
-        choices=[5000, 1000, 500, 100, 70],
-        help='frequency measure (ms)')
+        '-voltage', action='store_true',
+        help='voltage measure')
+    group_wsn430_consumption.add_argument(
+        '-current', action='store_true',
+        help='current measure')
 
+    # WSN430 Radio
     group_wsn430_radio = add_wsn430_parser.add_argument_group(
         'WSN430 radio measure')
-
+    group_wsn430_radio.add_argument(
+        '-rfreq', dest='rfreq', type=int,
+        choices=ProfileWSN430.choices['radio']['frequency'],
+        help='frequency measure (ms)')
     group_wsn430_radio.add_argument(
         '-rssi', action='store_true',
         help='RSSI measure')
 
-    group_wsn430_radio.add_argument(
-        '-rfreq', dest='rfreq', type=int,
-        choices=[5000, 1000, 500],
-        help='frequency measure (ms)')
-
+    # WSN430 Sensor
     group_wsn430_sensor = add_wsn430_parser.add_argument_group(
         'WSN430 sensor measure')
+    group_wsn430_sensor.add_argument(
+        '-sfreq', dest='sfreq', type=int,
+        choices=ProfileWSN430.choices['sensor']['frequency'],
+        help='frequency measure (ms)')
 
     group_wsn430_sensor.add_argument(
         '-temperature', action='store_true',
         help='temperature measure')
-
     group_wsn430_sensor.add_argument(
         '-luminosity', action='store_true',
         help='luminosity measure')
 
-    group_wsn430_sensor.add_argument(
-        '-sfreq', dest='sfreq', type=int,
-        choices=[30000, 10000, 5000, 1000],
-        help='frequency measure (ms)')
+    # Delete Profile
+    del_parser.add_argument('-n', '--name', required=True, help='profile name')
 
-    del_parser.add_argument(
-        '-n', '--name', dest='name', required=True,
-        help='profile name')
-
+    # Get Profile
     get_group = get_parser.add_mutually_exclusive_group(required=True)
 
-    get_group.add_argument(
-        '-n', '--name',
-        dest='name', help='profile name')
+    get_group.add_argument('-n', '--name', help='profile name')
 
     get_group.add_argument(
-        '-l', '--list', action='store_true', dest='list',
+        '-l', '--list', action='store_true',
         help='print profile\'s list JSON representation')
 
+    # Load Profile
     load_parser.add_argument(
         '-f', '--file', dest='path_file', required=True,
         help='profile JSON representation path file')
@@ -196,44 +188,22 @@ def parse_options():
     return parser
 
 
-def add_wsn430_profile(api, opts):
+def add_wsn430_profile_parser(api, opts):
     """ Add WSN430 user profile with JSON Encoder serialization object Profile.
 
+    :param api: API Rest api object
     :param opts: command-line parser opts
     :type opts:  Namespace object with opts attribute
-    :param api: API Rest api object
     """
-    consumption = None
-    radio = None
-    sensor = None
 
-    if opts.current or opts.voltage or opts.power:
-        if opts.cfreq is None:
-            raise Error(
-                "You must specify a frequency for consumption measure.")
-        consumption = Consumption(current=opts.current,
-                                  voltage=opts.voltage,
-                                  power=opts.power,
-                                  frequency=opts.cfreq)
-
-    if opts.rssi:
-        if opts.rfreq is None:
-            raise Error("You must specify a frequency for radio measure.")
-        radio = Radio(rssi=opts.rssi, frequency=opts.rfreq)
-
-    if opts.luminosity or opts.temperature:
-        if opts.sfreq is None:
-            raise Error("You must specify a frequency for sensor measure.")
-        sensor = Sensor(temperature=opts.temperature,
-                        luminosity=opts.luminosity,
-                        frequency=opts.sfreq)
-
-    profile = Profile(nodearch='wsn430',
-                      profilename=opts.name,
-                      power=opts.power_mode,
-                      consumption=consumption,
-                      radio=radio,
-                      sensor=sensor)
+    try:
+        profile = ProfileWSN430(opts.name, opts.power_mode)
+        profile.set_radio(opts.rfreq, opts.rssi)
+        profile.set_consumption(opts.cfreq, opts.power, opts.voltage,
+                                opts.current)
+        profile.set_sensors(opts.sfreq, opts.temperature, opts.luminosity)
+    except AssertionError as err:
+        raise ValueError(err.message)
 
     if opts.json:
         return profile
@@ -241,48 +211,22 @@ def add_wsn430_profile(api, opts):
         return api.add_profile(opts.name, profile)
 
 
-def add_m3_profile(api, opts):
+def add_m3_profile_parser(api, opts):
     """ Add M3 user profile with JSON Encoder serialization object Profile.
 
+    :param api: API Rest api object
     :param opts: command-line parser opts
     :type opts:  Namespace object with opts attribute
-    :param api: API Rest api object
     """
-    consumption = None
-    radio = None
 
-    if opts.current or opts.voltage or opts.cpower:
-        if opts.period is not None and opts.average is not None:
-            consumption = Consumption(current=opts.current,
-                                      voltage=opts.voltage,
-                                      power=opts.cpower,
-                                      period=opts.period,
-                                      average=opts.average)
-        else:
-            raise Error("You must specify values for period/average"
-                        " consumption measure.")
-
-    if opts.rssi:
-        if opts.rperiod is None or opts.channels is None:
-            raise Error("You must specify values for "
-                        "channels/period radio measure.")
-
-        if opts.num_per_channel is None and len(opts.channels) > 1:
-            raise Error("You must specify value for "
-                        "number of measure by channel.")
-        elif len(opts.channels) == 1:
-            opts.num_per_channel = 0
-
-        radio = Radio(mode="rssi",
-                      period=opts.rperiod,
-                      num_per_channel=opts.num_per_channel,
-                      channels=opts.channels)
-
-    profile = Profile(nodearch='m3',
-                      profilename=opts.name,
-                      power=opts.power_mode,
-                      consumption=consumption,
-                      radio=radio)
+    try:
+        profile = ProfileM3(opts.name, opts.power_mode)
+        profile.set_consumption(opts.period, opts.average,
+                                opts.power, opts.voltage, opts.current)
+        profile.set_radio('rssi', opts.channels, opts.rperiod,
+                          opts.num_per_channel)
+    except AssertionError as err:
+        raise ValueError(err.message)
 
     if opts.json:
         return profile
@@ -290,21 +234,22 @@ def add_m3_profile(api, opts):
         return api.add_profile(opts.name, profile)
 
 
-def load_profile(api, path_file):
+def load_profile_parser(api, path_file):
     """  Load and add user profile description
 
+    :param api: API Rest api object
     :param path_file: path file profile description
     :type path_file: string
-    :param api: API Rest api object
     """
-    file_data = helpers.read_file(path_file)
-    profile = json.loads(file_data)
+    profile = json.loads(helpers.read_file(path_file))
     if "profilename" not in profile:
-        raise Error("You must have a profilename attribute in your JSON file")
+        raise ValueError("'profilename' attribute required in JSON file: %r" %
+                         path_file)
+
     return api.add_profile(profile["profilename"], profile)
 
 
-def del_profile(api, name):
+def del_profile_parser(api, name):
     """ Delete user profile description
 
     :param name: profile name
@@ -314,7 +259,7 @@ def del_profile(api, name):
     return api.del_profile(name)
 
 
-def get_profile(api, opts):
+def get_profile_parser(api, opts):
     """ Get user profile description
     _ print JSONObject profile description
     _ print JSONObject profile's list description
@@ -337,15 +282,15 @@ def profile_parse_and_run(opts):
 
     subparser_name = opts.subparser_name
     if subparser_name == 'addwsn430':
-        result = add_wsn430_profile(api, opts)
+        result = add_wsn430_profile_parser(api, opts)
     elif subparser_name == 'addm3':
-        result = add_m3_profile(api, opts)
+        result = add_m3_profile_parser(api, opts)
     elif subparser_name == 'load':
-        result = load_profile(api, opts.path_file)
+        result = load_profile_parser(api, opts.path_file)
     elif subparser_name == 'get':
-        result = get_profile(api, opts)
+        result = get_profile_parser(api, opts)
     elif subparser_name == 'del':
-        result = del_profile(api, opts.name)
+        result = del_profile_parser(api, opts.name)
 
     return result
 
