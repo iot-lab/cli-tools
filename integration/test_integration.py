9e1115233ee6e0b420b4a238fefd5d1de51fd9c9
Gaëtan Harter <gaetan.harter@inria.fr>
2014-10-02 14:26:39 +0200

    Add profile tests. Run tests with user 'iotlab'

--- integration/test_integration.py
+++ integration/test_integration.py
@@ -1,5 +1,8 @@
 """ Integration tests for cli-tools """
 
+# pylint:disable=too-many-public-methods
+# pylint:disable=attribute-defined-outside-init
+
 from __future__ import print_function
 import os
 import sys
@@ -8,6 +11,8 @@ import shlex
 import time
 import runpy
 import unittest
+
+from tempfile import NamedTemporaryFile
 
 try:
     # pylint:disable=F0401,E0611
@@ -28,15 +33,16 @@ class TestCliToolsExperiments(unittest.TestCase):
         self.firmwares = firmwares
         self.exp_desc = call_cli(cmd + ' --print')
         self.exp_id = call_cli(cmd, "id")
-        self.id = ' --id {} '.format(self.exp_id)
+        self.id_str = ' --id {} '.format(self.exp_id)
         print(self.exp_id)
 
     def _get_exp_info(self):
         """ Get experiment info and check them """
-        cmd = 'experiment-cli get --print' + self.id
-        self.deploymentresults = call_cli(cmd, 'deploymentresults')
+        cmd = 'experiment-cli get --print' + self.id_str
+        exp_json = call_cli(cmd)
+        # TODO check returned values
         # there should be successfull deployements
-        self.assertNotEquals([], self.deploymentresults['0'])
+        self.assertNotEquals([], exp_json['deploymentresults']['0'])
 
         cmd = 'experiment-cli get --resources-id -i {}'.format(self.exp_id)
         call_cli(cmd)
@@ -62,7 +68,7 @@ class TestCliToolsExperiments(unittest.TestCase):
         ret = call_cli(cmd)
         print(ret['status'])
 
-        state = self._wait_state_or_finished()
+        self._wait_state_or_finished()
 
     def _wait_state_or_finished(self, state=None):
         """ Wait experiment get in state, or states error and terminated """
@@ -104,6 +110,80 @@ class TestCliToolsExperiments(unittest.TestCase):
             call_cli('experiment-cli stop -i {}'.format(exp_id))
 
 
+class TestCliToolsAProfile(unittest.TestCase):
+    """ Test the cli tools profile """
+    profile = {
+        'm3': 'test_cli_profile_m3',
+        'm3_full': 'test_cli_profile_m3_full',
+        'wsn430': 'test_cli_profile_wsn430',
+    }
+
+    @classmethod
+    def setUpClass(cls):
+        """ Remove the tests profiles if they are here """
+        remote_prof = call_cli('profile-cli get --list')
+        for prof in cls.profile.values():
+            if prof not in remote_prof:
+                continue
+            call_cli('profile-cli del --name {}'.format(prof['profilename']))
+
+    def _add_profile_simple(self, cmd, name):
+        """ Add a profile and get it to check it's the same """
+        profile_dict = call_cli(cmd + ' --json')
+
+        # add profile return name
+        self.assertEquals(name, call_cli(cmd))
+
+        get_profile_dict = call_cli('profile-cli get --name {}'.format(name))
+        # Don't break with new features
+        self.assertLessEqual(profile_dict, get_profile_dict)
+
+    def _get_and_load(self, name):
+        """ Get a profile and try loading it
+        We then check that getting both profiles return the same output
+        """
+
+        get_profile_dict = call_cli('profile-cli get --name {}'.format(name))
+        with NamedTemporaryFile(mode='w+') as prof:
+            prof.write(json.dumps(get_profile_dict))
+            prof.flush()
+            l_name = call_cli('profile-cli load --file {}'.format(prof.name))
+        # returned name are the same
+        self.assertEquals(l_name, name)
+        get_loaded_profile = call_cli('profile-cli get --name {}'.format(name))
+        # returned profile are the same
+        self.assertEquals(get_profile_dict, get_loaded_profile)
+
+    def _add_profile(self, cmd, name):
+        """ Test adding and loading a user profile """
+        self._add_profile_simple(cmd.format(name), name)
+        self._get_and_load(name)  # erase same profile
+
+    def test_m3_profile(self):
+        """ Test creating an M3 profile and deleting it """
+
+        profs = call_cli('profile-cli get -l')
+        profiles_names = set([p['profilename'] for p in profs])
+
+        self._add_profile('profile-cli addm3 -n {} -p dc', self.profile['m3'])
+        profiles_names.add(self.profile['m3'])
+
+        prof_cmd = 'profile-cli addm3 -n {} -p dc'
+        prof_cmd += ' -power -voltage -current -period 8244 -avg 1024'
+        prof_cmd += ' -rssi -channels 11 16 21 26 -num 255 -rperiod 65535'
+        self._add_profile(prof_cmd, self.profile['m3_full'])
+        profiles_names.add(self.profile['m3_full'])
+
+        # check that profiles have been added
+        profs = call_cli('profile-cli get -l')
+        profiles_names_new = set([p['profilename'] for p in profs])
+        self.assertEquals(profiles_names, profiles_names_new)
+
+        # ret == ''
+        call_cli('profile-cli del --name {}'.format(self.profile['m3']))
+        call_cli('profile-cli del --name {}'.format(self.profile['m3_full']))
+
+
 def call_cli(cmd, field=None):
     """ Call cli tool """
     argv = shlex.split(cmd)
@@ -123,7 +203,17 @@ def call_cli(cmd, field=None):
     return ret
 
 
+def setup_auth(username, password):
+    """ Setup the username password for test user """
+    call_cli('auth-cli --user {} --password {}'.format(username, password))
+
+
 if __name__ == '__main__':
-    # TODO Setup with auth_cli
-    # with patch('iotlabcli.auth.RC_FILE', 'test_auth_file'):
+    USERNAME = "iotlab"
+    PASSWORD = os.getenv('IOTLAB_TEST_PASSWORD')
+    assert os.getenv('IOTLAB_TEST_PASSWORD') is not None
+
+    os.environ['IOTLAB_PASSWORD_FILE'] = 'test_auth_file'
+
+    setup_auth(USERNAME, PASSWORD)
     unittest.main()
