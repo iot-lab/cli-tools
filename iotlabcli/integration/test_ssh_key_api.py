e5260a39a173151f3f8bfa710c9db9d290a5a93d
Olivier Fambon <olivier.fambon@inria.fr>
2017-02-07 00:05:19 +0100

    tests: added test_install_key_*()

--- iotlabcli/integration/test_ssh_key_api.py
+++ iotlabcli/integration/test_ssh_key_api.py
@@ -35,6 +35,40 @@ def test_ssh_keys_overflow(api):
     max_5_keys["sshkeys"].pop()
     assert res == max_5_keys
 
+def test_install_key_user_key(api, user_key):
+    clear_keys(api)
+    api.install_ssh_key()
+    res = api.get_ssh_keys()
+    res = res["sshkeys"]
+    assert res[0] == user_key
+
+def test_install_key_user_key_again(api, user_key):
+    clear_keys(api)
+    api.install_ssh_key()
+    api.install_ssh_key()
+    res = api.get_ssh_keys()
+    res = res["sshkeys"]
+    assert res[0] == user_key
+    assert len(res) == 1
+
+def test_install_key_from_file(api, user_key):
+    clear_keys(api)
+    api.install_ssh_key("~/.ssh/id_rsa.pub")
+    res = api.get_ssh_keys()
+    res = res["sshkeys"]
+    assert res[0] == user_key
+
+def test_install_key_round_robin(api, user_key):
+    max_5_keys = {"sshkeys": [ "key-"+str(i) for i in range(5) ]}
+    api.set_ssh_keys(max_5_keys)
+
+    api.install_ssh_key()
+    res = api.get_ssh_keys()
+    res = res["sshkeys"]
+    assert res[4] == user_key
+    assert res[3] == "key-4"
+
+
 @pytest.fixture(scope="module")
 def api():
     user, password = auth.get_user_credentials()
@@ -45,3 +79,12 @@ def save_orig_keys(api):
     orig_keys = api.get_ssh_keys()
     yield orig_keys
     api.set_ssh_keys(orig_keys)
+
+@pytest.fixture
+def clear_keys(api):
+    no_keys = {"sshkeys": []}
+    api.set_ssh_keys(no_keys)
+
+@pytest.fixture
+def user_key():
+    return open(rest.ssh_key.KEY_FILE).read()
