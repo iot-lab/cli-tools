223f438a06d5cc9491293f81725cdd6971eb4f57
Olivier Fambon <olivier.fambon@inria.fr>
2017-02-06 23:20:39 +0100

    tests: use scope=module for api() and save_orig_keys() fixtures

--- iotlabcli/integration/test_ssh_key_api.py
+++ iotlabcli/integration/test_ssh_key_api.py
@@ -3,6 +3,8 @@ import pytest
 from iotlabcli import rest
 from iotlabcli import auth
 
+pytestmark = pytest.mark.usefixtures("save_orig_keys")
+
 
 def test_get_ssh_keys(api):
     api.get_ssh_keys()
@@ -13,32 +15,32 @@ def test_set_ssh_keys(api):
     new_keys = api.get_ssh_keys()
     assert new_keys == keys_json
 
-def test_clear_ssh_keys(api, save_orig_keys):
+def test_clear_ssh_keys(api):
     no_keys = {"sshkeys": []}
     api.set_ssh_keys(no_keys)
     res = api.get_ssh_keys()
     res = res["sshkeys"]
     assert res[0] == ""
 
-def test_max_ssh_keys(api, save_orig_keys):
+def test_max_ssh_keys(api):
     max_5_keys = {"sshkeys": [ "key-"+str(i) for i in range(5) ]}
     api.set_ssh_keys(max_5_keys)
     res = api.get_ssh_keys()
     assert res == max_5_keys
 
-def test_ssh_keys_overflow(api, save_orig_keys):
+def test_ssh_keys_overflow(api):
     max_5_keys = {"sshkeys": [ "key-"+str(i) for i in range(6) ]}
     api.set_ssh_keys(max_5_keys)
     res = api.get_ssh_keys()
     max_5_keys["sshkeys"].pop()
     assert res == max_5_keys
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def api():
     user, password = auth.get_user_credentials()
     return rest.Api(user, password)
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def save_orig_keys(api):
     orig_keys = api.get_ssh_keys()
     yield orig_keys
