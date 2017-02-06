0d3b69a676c637732356127e3831e957a10a9aa0
Olivier Fambon <olivier.fambon@inria.fr>
2017-02-06 20:40:22 +0100

    tests: added save_orig_keys() fixture

--- iotlabcli/integration/test_ssh_key_api.py
+++ iotlabcli/integration/test_ssh_key_api.py
@@ -13,18 +13,21 @@ def test_set_ssh_keys(api):
     new_keys = api.get_ssh_keys()
     assert new_keys == keys_json
 
-def test_clear_ssh_keys(api):
-    orig_keys = api.get_ssh_keys()
-
+def test_clear_ssh_keys(api, save_orig_keys):
     no_keys = {"sshkeys": []}
     api.set_ssh_keys(no_keys)
     res = api.get_ssh_keys()
     res = res["sshkeys"]
     assert res[0] == ""
 
-    api.set_ssh_keys(orig_keys)
 
 @pytest.fixture
 def api():
     user, password = auth.get_user_credentials()
     return rest.Api(user, password)
+
+@pytest.fixture
+def save_orig_keys(api):
+    orig_keys = api.get_ssh_keys()
+    yield orig_keys
+    api.set_ssh_keys(orig_keys)
