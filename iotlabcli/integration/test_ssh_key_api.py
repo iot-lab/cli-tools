8d1f13431b8ece8623f03651222904678b8b9a53
Olivier Fambon <olivier.fambon@inria.fr>
2017-02-06 20:04:15 +0100

    tests: added test_clear_ssh_keys()

--- iotlabcli/integration/test_ssh_key_api.py
+++ iotlabcli/integration/test_ssh_key_api.py
@@ -11,6 +11,16 @@ def test_set_ssh_keys(api):
     keys_json = api.get_ssh_keys()
     api.set_ssh_keys(keys_json)
 
+def test_clear_ssh_keys(api):
+    orig_keys = api.get_ssh_keys()
+
+    no_keys = {"sshkeys": []}
+    api.set_ssh_keys(no_keys)
+    res = api.get_ssh_keys()
+    res = res["sshkeys"]
+    assert res[0] == ""
+
+    api.set_ssh_keys(orig_keys)
 
 @pytest.fixture
 def api():
