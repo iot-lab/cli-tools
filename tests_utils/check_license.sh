adaf54f0963ff65d54b1155448b0f67978143b97
Henry-Joseph Audéoud <henry-joseph.audeoud@univ-grenoble-alpes.fr>
2018-05-17 16:42:08 +0200

    utils: add bash completion script

--- tests_utils/check_license.sh
+++ tests_utils/check_license.sh
@@ -26,6 +26,7 @@ files_list=$(echo "${files_list}" | grep -v \
     -e 'examples/' \
     -e '.travis.yml' \
     -e 'codecov.yml' \
+    -e 'utils/iotlabcli-bash-completion.sh' \
 )
 
 # Verify that 'AUTHORS' and 'COPYING' files exist
