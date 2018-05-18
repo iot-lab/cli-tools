ef0ac6e20543934e8058ec818e3c00dea910655f
Henry-Joseph Audéoud <henry-joseph.audeoud@univ-grenoble-alpes.fr>
2018-05-18 14:49:37 +0200

    Support iotlab-ssh in bash-completion script

--- utils/iotlab-cli-tools-bash-completion.sh
+++ utils/iotlab-cli-tools-bash-completion.sh
@@ -459,6 +459,112 @@ _iotlab_robot() {
     esac
 }
 
+_iotlab_ssh() {
+    local cur prev words cword
+    _init_completion || return
+
+    case $prev in
+        -v|--version|-h|--help)
+            return 0
+            ;;
+        -u|--user|-p|--password)
+            return 0
+            ;;
+    esac
+
+    # Look for the command name
+    local subcword cmd
+    for (( subcword=1; subcword < ${#words[@]}-1; subcword++ )); do
+        [[ ${words[subcword]} != -* && \
+            ! ${words[subcword-1]} =~ -+(jmespath|jp|format|fmt|u(ser)?|p(assword)) ]] && \
+                { cmd=${words[subcword]}; break; }
+    done
+
+    if [[ -z $cmd ]]; then
+        case $cur in
+            -*)
+                # No command name, complete with generic flags
+                COMPREPLY=($(compgen -W '-h --help -u --user -p --password -v --version --jmespath --jp --format --fmt -i --id --verbose' -- "$cur" ))
+                return 0
+                ;;
+            *)
+                # Complete with a command name
+                COMPREPLY=($(compgen -W 'flash-m3 reset-m3 wait-for-boot run-script run-cmd copy-file' -- "$cur"))
+                return 0
+                ;;
+        esac
+    fi
+
+    # Complete command arguments
+    case $cmd in
+        flash-m3|copy-file)
+            case "$prev" in
+                -u|--user|-p|--password)
+                    # Nothing to complete
+                    ;;
+                -e|--exclude)
+                    _iotlab_resources_list
+                    ;;
+                -l|--list)
+                    _iotlab_resources_list
+                    ;;
+                -*)
+                    COMPREPLY=($(compgen -W '-h --help -u --user -p --password -v --version -e --exclude -l --list' -- "$cur" ))
+                    ;;
+                *)
+                    _filedir
+            esac
+            ;;
+        reset-m3)
+            case "$prev" in
+                -u|--user|-p|--password)
+                    # Nothing to complete
+                    ;;
+                -e|--exclude)
+                    _iotlab_resources_list
+                    ;;
+                -l|--list)
+                    _iotlab_resources_list
+                    ;;
+                *)
+                    COMPREPLY=($(compgen -W '-h --help -u --user -p --password -v --version -e --exclude -l --list' -- "$cur" ))
+            esac
+            ;;
+        wait-for-boot)
+            case "$prev" in
+                -u|--user|-p|--password|--max-wait)
+                    # Nothing to complete
+                    ;;
+                -e|--exclude)
+                    _iotlab_resources_list
+                    ;;
+                -l|--list)
+                    _iotlab_resources_list
+                    ;;
+                *)
+                    COMPREPLY=($(compgen -W '-h --help -u --user -p --password -v --version --max-wait -e --exclude -l --list' -- "$cur" ))
+            esac
+            ;;
+        run-script|run-cmd)
+            case "$prev" in
+                -u|--user|-p|--password)
+                    # Nothing to complete
+                    ;;
+                -e|--exclude)
+                    _iotlab_resources_list
+                    ;;
+                -l|--list)
+                    _iotlab_resources_list
+                    ;;
+                -*)
+                    COMPREPLY=($(compgen -W '-h --help -u --user -p --password -v --version --frontend -e --exclude -l --list' -- "$cur" ))
+                    ;;
+                *)
+                    _filedir
+            esac
+    esac
+}
+
 complete -F _iotlab_admin iotlab-admin
 complete -F _iotlab_admin admin-cli
 complete -F _iotlab_auth iotlab-auth
@@ -471,6 +577,5 @@ complete -F _iotlab_profile iotlab-profile
 complete -F _iotlab_profile profile-cli
 complete -F _iotlab_robot iotlab-robot
 complete -F _iotlab_robot robot-cli
-# TODO
-#complete -F _iotlab_ssh iotlab-ssh
-#complete -F _iotlab_ssh ssh-cli
+complete -F _iotlab_ssh iotlab-ssh
+complete -F _iotlab_ssh ssh-cli
