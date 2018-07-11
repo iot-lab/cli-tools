537d14a1909b4ea9c40b941ec44ceeb8de7bbc90
Matthieu Berthomé <matthieu.berthome@inria.fr>
2018-07-11 17:16:43 +0200

    adaptation to new endpoints etc

--- tests_utils/robots_circuits.py
+++ tests_utils/robots_circuits.py
@@ -1,5 +1,6 @@
 import json
 from urllib2 import HTTPError
+from urlparse import urljoin
 
 import iotlabcli
 import pprint
@@ -22,19 +23,37 @@ def delete_if_exists(circuit_name):
 
 # get all circuits
 print('all circuits:')
-pprint.pprint(api.method('mobilities/circuits', method='get', params={'filter': 'all'}))
+pprint.pprint(api.method('mobilities/circuits', method='get', params={'type': 'all'}))
 print('userdefined circuits:')
-pprint.pprint(api.method('mobilities/circuits', method='get', params={'filter': 'userdefined'}))
+pprint.pprint(api.method('mobilities/circuits', method='get', params={'type': 'userdefined'}))
 print('predefined circuits:')
-pprint.pprint(api.method('mobilities/circuits', method='get', params={'filter': 'predefined'}))
+pprint.pprint(api.method('mobilities/circuits', method='get', params={'type': 'predefined'}))
+print('predefined circuits on devlille:')
+pprint.pprint(api.method('mobilities/circuits', method='get', params={'site':'devlille', 'type': 'predefined'}))
+
+print('predefined circuit square1 on devlille:')
+pprint.pprint(api.method('mobilities/circuits/square1', method='get'))
 
 print('add my_circuit user defined circuit:')
 delete_if_exists('my_circuit')
 circuit = json.load(open('my_circuit.json', 'r'))
 pprint.pprint(api.method('mobilities/circuits', method='post', json=circuit))
 
+# remove point B in the circuit
+del circuit["coordinates"]['B']
+circuit['points'].remove('B')
+
+req = api._request(urljoin(api.url, 'mobilities/circuits/my_circuit'), method='put', auth=api.auth, json=circuit)
+
+
+print('userdefined circuit my_circuit on devlille:')
+pprint.pprint(api.method('mobilities/circuits/my_circuit', method='get'))
+
 print('userdefined circuits:')
 pprint.pprint(api.method('mobilities/circuits', method='get', params={'filter': 'userdefined'}))
+
+print('userdefined circuits on devstrasbourg:')
+pprint.pprint(api.method('mobilities/circuits', method='get', params={'site':'devstrasbourg', 'filter': 'userdefined'}))
 
 print('get map config:')
 pprint.pprint(api.method('robots/%s/map/config' % site, method='get'))
@@ -42,26 +61,23 @@ pprint.pprint(api.method('robots/%s/map/config' % site, method='get'))
 print('get dock config:')
 pprint.pprint(api.method('robots/%s/dock/config' % site, method='get'))
 
-print('is_reachable:')
-pprint.pprint(api.method('robots/%s/is_reachable' % site,
-                         json={'x': 20, 'y': 4},
+print('reachable:')
+pprint.pprint(api.method('robots/%s/coordinates/isreachable' % site,
+                         json={'A':{'x': 20, 'y': 4}},
                          method='post'))
-print('are_reachable:')
-pprint.pprint(api.method('robots/%s/are_reachable' % site,
-                         json=[{'x': 20, 'y': 4}, {'x': 20, 'y': 5}],
-                         method='post'))
+
 
 print('original_coordinates:')
 original_coordinates = {'x': 20, 'y': 4, }
 pprint.pprint(original_coordinates)
 
 print('to_map_coordinates:')
-to_map_coordinates = api.method('robots/%s/coordinates/to_map_coordinates' % site,
+to_map_coordinates = api.method('robots/%s/coordinates/map' % site,
                                 json=original_coordinates,
                                 method='post')
 pprint.pprint(to_map_coordinates)
 
 print('back to_ros_coordinates:')
-pprint.pprint(api.method('robots/%s/coordinates/to_ros_coordinates' % site,
+pprint.pprint(api.method('robots/%s/coordinates/ros' % site,
                          json=to_map_coordinates,
                          method='post'))
